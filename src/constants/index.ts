28749c94fa664a090e90cb9334299d8b9211817b
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-08-19 21:08:22 +0300

    feat: add auth constants

---
+++ src/constants/index.ts
@@ -0,0 +1,29 @@
+export const MODAL_TITLES = {
+  signIn: 'Вход',
+  signUp: 'Регистрация',
+};
+
+export const AUTH_FORM = {
+  email: {
+    label: 'Электронная почта',
+    name: 'email',
+    placeholder: 'user@email.com',
+  },
+  password: {
+    label: 'Пароль',
+    name: 'password',
+    placeholder: '********',
+  },
+};
+
+export const BUTTON_TEXT = {
+  signIn: 'Войти',
+  signUp: 'Зарегистрироваться',
+};
+
+export const REDIRECTION_LINK_TEXT = {
+  signIn: 'У меня уже есть аккаунт',
+  signUp: 'Создать аккаунт',
+};
+
+export const NO_CONTENT = '';
