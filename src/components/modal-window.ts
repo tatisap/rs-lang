80e0b4fe2a2a4a4547d893f0fcfcca80afc424f4
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-08-19 21:05:52 +0300

    feat: implement modal-window component

---
+++ src/components/modal-window.ts
@@ -0,0 +1,62 @@
+import { NO_CONTENT } from '../constants';
+import CommonView from './common/view';
+
+export default class ModalWindow {
+  private elementCreator: CommonView;
+
+  public modalWrapper: HTMLDivElement;
+
+  public modalContent: HTMLDivElement;
+
+  public closeButton: HTMLButtonElement;
+
+  public titleElement: HTMLHeadingElement;
+
+  constructor(modalTitle: string) {
+    this.elementCreator = new CommonView();
+    this.modalWrapper = this.elementCreator.createUIElement<HTMLDivElement>({
+      tag: 'div',
+      classNames: ['modal__wrapper'],
+    });
+    const modalElement = this.elementCreator.createUIElement<HTMLDivElement>({
+      tag: 'div',
+      classNames: ['modal'],
+    });
+    this.closeButton = this.elementCreator.createUIElement<HTMLButtonElement>({
+      tag: 'button',
+      classNames: ['close-button'],
+    });
+    this.closeButton.addEventListener('click', (): void => this.close());
+    this.titleElement = this.elementCreator.createUIElement<HTMLHeadingElement>({
+      tag: 'h3',
+      classNames: ['modal__title'],
+      innerText: modalTitle,
+    });
+    this.modalContent = this.elementCreator.createUIElement<HTMLDivElement>({
+      tag: 'div',
+      classNames: ['modal__content'],
+    });
+    modalElement.append(this.closeButton, this.titleElement, this.modalContent);
+    this.modalWrapper.append(modalElement);
+  }
+
+  public open(): void {
+    document.body.append(this.modalWrapper);
+  }
+
+  public close(): void {
+    this.modalWrapper.remove();
+  }
+
+  public clear(): void {
+    this.modalContent.innerHTML = NO_CONTENT;
+  }
+
+  public setModalTitle(modalTitle: string): void {
+    this.titleElement.textContent = modalTitle;
+  }
+
+  public addContent(...content: HTMLElement[]): void {
+    this.modalContent.append(...content);
+  }
+}
