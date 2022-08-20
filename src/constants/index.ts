c390972f52a3acf24c1739c4295a722a766e542a
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-20 05:48:20 +0300

    feat: add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,66 @@
+export const PAGE_TITLES = {
+  about: 'Наша команда',
+  studentBook: 'Учебник',
+  games: 'Игры',
+  statistics: 'Статистика',
+};
+
+export const GAMES = {
+  audiocall: {
+    name: 'Аудиовызов',
+    className: 'audiocall',
+    link: '',
+  },
+  sprint: {
+    name: 'Спринт',
+    className: 'sprint',
+    link: '',
+  },
+};
+
+export const BOOK_SECTIONS = {
+  beginner: {
+    text: 'A1',
+    className: 'a1',
+    color: '#FFDFDF',
+  },
+  elementary: {
+    text: 'A2',
+    className: 'a2',
+    color: '#FDDEC1',
+  },
+  intermediate: {
+    text: 'B1',
+    className: 'b1',
+    color: '#FFFCBE',
+  },
+  upperIntermediate: {
+    text: 'B2',
+    className: 'b2',
+    color: '#DBFFC5',
+  },
+  advanced: {
+    text: 'C1',
+    className: 'c1',
+    color: '#DDFBFF',
+  },
+  proficient: {
+    text: 'C2',
+    className: 'c2',
+    color: '#D5EAFE',
+  },
+  difficultWords: {
+    text: 'Superhero',
+    className: 'superhero',
+    color: '#E2DDFF',
+  },
+};
+
+export const PAGINATION_BUTTONS = {
+  previous: {
+    className: 'button-previous',
+  },
+  next: {
+    className: 'button-next',
+  },
+};
