26bbaaa78c8afd97b158dbe2dc12f95830549ba2
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-20 06:54:38 +0300

    fix: update book sections constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -23,31 +23,37 @@ export const BOOK_SECTIONS = {
     text: 'A1',
     className: 'a1',
     color: '#FFDFDF',
+    group: 0,
   },
   elementary: {
     text: 'A2',
     className: 'a2',
     color: '#FDDEC1',
+    group: 1,
   },
   intermediate: {
     text: 'B1',
     className: 'b1',
     color: '#FFFCBE',
+    group: 2,
   },
   upperIntermediate: {
     text: 'B2',
     className: 'b2',
     color: '#DBFFC5',
+    group: 3,
   },
   advanced: {
     text: 'C1',
     className: 'c1',
     color: '#DDFBFF',
+    group: 4,
   },
   proficient: {
     text: 'C2',
     className: 'c2',
     color: '#D5EAFE',
+    group: 5,
   },
   difficultWords: {
     text: 'Superhero',
