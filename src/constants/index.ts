f1f312cb4f746d26f3437afff94e6c19d706fc41
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-08-20 15:02:02 +0300

    feat: add http constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,30 @@
+import { AuthMode } from '../types';
+
+export const BASE_URL = 'http://127.0.0.1:4000';
+
+export const PATHS = {
+  words: 'words',
+  users: 'users',
+  signIn: 'signin',
+  tokens: 'tokens',
+  aggregatedWords: 'aggregatedWords',
+  statistics: 'statistics',
+  settings: 'settings',
+};
+
+export const QUERY_KEYS = {
+  group: 'group',
+  page: 'page',
+  wordsPerPage: 'wordsPerPage',
+  filter: 'filter',
+};
+
+export const REQUEST_HEADERS = {
+  contentType: 'Content-Type',
+  accept: 'Accept',
+  authorization: 'Authorization',
+};
+
 export const MODAL_TITLES = {
   signIn: 'Вход',
   signUp: 'Регистрация',
@@ -27,3 +54,18 @@ export const REDIRECTION_LINK_TEXT = {
 };
 
 export const NO_CONTENT = '';
+
+export const STORAGE_KEYS = {
+  user: '77-user',
+};
+
+export const AUTH_MODAL_MODES: { [K in AuthMode]: AuthMode } = {
+  signIn: 'signIn',
+  signUp: 'signUp',
+};
+
+export const DEFAULT_AUTH_MODAL_MODE = 'signIn';
+
+export const PAGE_TITLES = {
+  about: 'fff',
+};
