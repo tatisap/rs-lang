4c6782d3320016abd53b876ba5391966f9e10d3c
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-23 12:06:35 +0300

    feat: implement setting and getting local storage to show last opened section and page

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,10 +1,11 @@
 import { DEFAULT_PAGE_NAME, NO_CONTENT, STORAGE_KEYS } from '../constants';
-import { PageName } from '../types';
+import { IBookSectionInfo, PageName } from '../types';
 import Auth from './auth/auth';
 import GamesHomepage from './games-homepage';
 import MainPageView from './main-page/view';
 import Menu from './menu';
 import StudentBookView from './student-book/view';
+import StudentBookController from './student-book/controller';
 
 export default class App {
   private menu: Menu;
@@ -17,6 +18,8 @@ export default class App {
     games: GamesHomepage;
   };
 
+  private studentBookController: StudentBookController;
+
   constructor() {
     this.menu = new Menu();
     this.auth = new Auth();
@@ -25,15 +28,27 @@ export default class App {
       studentBook: new StudentBookView(),
       games: new GamesHomepage(),
     };
+    this.studentBookController = new StudentBookController();
   }
 
   public start(): void {
     this.menu.init((event: Event): void => this.menuHandler(event));
     this.auth.init();
 
+    window.addEventListener('beforeunload', (): void => {
+      this.studentBookController.saveSection();
+      this.studentBookController.savePage();
+    });
+
     const currentPage: PageName = this.getCurrentPageName();
+    const bookSection: IBookSectionInfo = this.studentBookController.getSection();
+    const bookPage: number = this.studentBookController.getPage();
+    if (currentPage === 'studentBook') {
+      this.openPage(currentPage, bookSection, bookPage);
+    } else {
+      this.openPage(currentPage);
+    }
     this.menu.setMenuItemActiveState(currentPage);
-    this.openPage(currentPage);
   }
 
   private menuHandler(event: Event): void {
@@ -47,9 +62,13 @@ export default class App {
     this.menu.closeMenu();
   }
 
-  private openPage(pageName: PageName): void {
+  private openPage(pageName: PageName, bookSection?: IBookSectionInfo, bookPage?: number): void {
     (document.querySelector('#app') as HTMLElement).innerHTML = NO_CONTENT;
-    this.pages[pageName].renderPage();
+    if (bookSection && bookPage) {
+      this.pages[pageName].renderPage(bookSection, bookPage);
+    } else {
+      this.pages[pageName].renderPage();
+    }
   }
 
   private saveCurrentPageName(pageName: PageName): void {
