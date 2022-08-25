02b4ba10cb74eceff5fce0648e883ca671789243
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-08-25 22:50:04 +0300

    feat: move game homepage ui components to the separate class

---
+++ src/components/games-homepage/games-homepage-constructor.ts
@@ -0,0 +1,68 @@
+import { GAMES, GAME_LIST_TITLE, PAGE_TITLES } from '../../constants';
+import { IGameInfo } from '../../types';
+import UIElementsConstructor from '../../utils/ui-elements-creator';
+
+export default class GamesHomepageConstructor {
+  private elementCreator: UIElementsConstructor;
+
+  constructor() {
+    this.elementCreator = new UIElementsConstructor();
+  }
+
+  public createPageTitle(): HTMLHeadingElement {
+    return this.elementCreator.createUIElement<HTMLHeadingElement>({
+      tag: 'h2',
+      classNames: ['page__title'],
+      innerText: PAGE_TITLES.games,
+    });
+  }
+
+  private createGameListTitle(): HTMLHeadingElement {
+    return this.elementCreator.createUIElement<HTMLHeadingElement>({
+      tag: 'h3',
+      classNames: ['game-list__title'],
+      innerText: GAME_LIST_TITLE,
+    });
+  }
+
+  private createGameItem(className: string, gameName: string): HTMLLIElement {
+    return this.elementCreator.createUIElement<HTMLLIElement>({
+      tag: 'li',
+      classNames: ['game-list__item', `${className}-game`],
+      innerText: gameName,
+    });
+  }
+
+  private createGameList(): HTMLUListElement {
+    const gameList: HTMLUListElement = this.elementCreator.createUIElement<HTMLUListElement>({
+      tag: 'ul',
+      classNames: ['game-list'],
+    });
+    gameList.append(
+      ...(Object.values(GAMES) as IGameInfo[]).map(
+        (game: IGameInfo): HTMLLIElement => this.createGameItem(game.className, game.name)
+      )
+    );
+    return gameList;
+  }
+
+  private createGameListImage(): HTMLDivElement {
+    return this.elementCreator.createUIElement<HTMLDivElement>({
+      tag: 'div',
+      classNames: ['game-list__image'],
+    });
+  }
+
+  public createGameListContainer(): HTMLDivElement {
+    const gameListContainer: HTMLDivElement = this.elementCreator.createUIElement<HTMLDivElement>({
+      tag: 'div',
+      classNames: ['game-list__container'],
+    });
+    gameListContainer.append(
+      this.createGameListTitle(),
+      this.createGameList(),
+      this.createGameListImage()
+    );
+    return gameListContainer;
+  }
+}
