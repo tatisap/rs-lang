4ee6810baa7625c8906babcd9eb2ecc5f431a2a9
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-25 21:50:22 +0300

    fix: update get words from api method

--- src/api/words-api.ts
+++ src/api/words-api.ts
@@ -1,9 +1,11 @@
-import { IWord } from '../types';
+import { IWord, Numbers } from '../types';
 import { BASE_URL, PATHS, QUERY_KEYS } from '../constants';
 
 export default class WordsAPI {
   public async getWords(group: number, page: number): Promise<IWord[]> {
-    const url = `${BASE_URL}/${PATHS.words}?${QUERY_KEYS.group}=${group}&${QUERY_KEYS.page}=${page}`;
+    const url = `${BASE_URL}/${PATHS.words}?${QUERY_KEYS.group}=${group}&${QUERY_KEYS.page}=${
+      page - Numbers.One
+    }`;
     const response = await fetch(url);
     const data: IWord[] = await response.json();
     return data;
