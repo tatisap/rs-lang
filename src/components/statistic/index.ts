af55de485e6efe85a26d86734aebab32511b6d55
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-08-30 00:39:26 +0300

    feat: implement render page method

--- src/components/statistic/index.ts
+++ src/components/statistic/index.ts
@@ -1,18 +1,31 @@
+import { IProcessedStatisticInfo } from '../../types';
+import StatisticPageController from './controller';
 import StatisticUIConstructor from './ui-constructor';
 
 export default class StatisticPage {
   private uiConstructor: StatisticUIConstructor;
 
+  private controller: StatisticPageController;
+
   constructor() {
     this.uiConstructor = new StatisticUIConstructor();
+    this.controller = new StatisticPageController();
   }
 
-  renderPage() {
+  public async renderPage() {
     const pageContainer: HTMLElement = document.getElementById('app') as HTMLElement;
     pageContainer.classList.add('page_statistic');
-    pageContainer.append(
-      this.uiConstructor.createPageTitle(),
-      this.uiConstructor.createChartsContainer()
+    const chartsContainer: HTMLDivElement = this.uiConstructor.createChartsContainer();
+
+    const statisticInfo: IProcessedStatisticInfo =
+      await this.controller.getProcessedStatisticInfo();
+
+    chartsContainer.append(
+      this.uiConstructor.createDailyGameChart(statisticInfo.dailyGameChartData),
+      this.uiConstructor.createDailyWordChart(statisticInfo.dailyWordsChartData),
+      this.uiConstructor.createAllTimeChart(statisticInfo.allTimeChartData)
     );
+
+    pageContainer.append(this.uiConstructor.createPageTitle(), chartsContainer);
   }
 }
