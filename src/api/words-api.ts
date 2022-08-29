5db333a1be4574eb124d7cc40f95f346b96cc491
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-08-29 13:59:20 +0300

    feat: add get user words method to words api

--- src/api/words-api.ts
+++ src/api/words-api.ts
@@ -1,5 +1,5 @@
-import { IWord, Numbers } from '../types';
-import { BASE_URL, PATHS, QUERY_KEYS } from '../constants';
+import { HttpMethods, IUserWord, IWord, Numbers } from '../types';
+import { BASE_URL, PATHS, QUERY_KEYS, REQUEST_HEADERS } from '../constants';
 
 export default class WordsAPI {
   public async getWords(group: number, page: number): Promise<IWord[]> {
@@ -8,6 +8,19 @@ export default class WordsAPI {
     }`;
     const response = await fetch(url);
     const data: IWord[] = await response.json();
+    return data;
+  }
+
+  public async getUserWords(userId: string, token: string): Promise<IUserWord[]> {
+    const url = `${BASE_URL}/${PATHS.users}/${userId}/${PATHS.words}`;
+    const response: Response = await fetch(url, {
+      method: HttpMethods.GET,
+      headers: {
+        [REQUEST_HEADERS.authorization]: `Bearer ${token}`,
+        [REQUEST_HEADERS.accept]: 'application/json',
+      },
+    });
+    const data: IUserWord[] = (await response.json()) as IUserWord[];
     return data;
   }
 }
