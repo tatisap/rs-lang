b28c5005f09f6d82de22675e7051dbfbce691f4d
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-09-02 00:29:29 +0300

    fix: update api method to get difficult words

--- src/api/words-api.ts
+++ src/api/words-api.ts
@@ -1,5 +1,5 @@
-import { IWord, Numbers } from '../types';
-import { BASE_URL, PATHS, QUERY_KEYS } from '../constants';
+import { IWord, Numbers, HttpMethods, IRequestParameters, IAggregatedWord } from '../types';
+import { BASE_URL, PATHS, QUERY_KEYS, REQUEST_HEADERS, MAX_WORDS_IN_BOOK } from '../constants';
 
 export default class WordsAPI {
   public async getWords(group: number, page: number): Promise<IWord[]> {
@@ -17,10 +17,20 @@ export default class WordsAPI {
     return data;
   }
 
-  public async getDifficultWords(userId: string): Promise<IWord[]> {
-    const url = `${BASE_URL}/${PATHS.users}/${userId}/${PATHS.aggregatedWords}?${QUERY_KEYS.filter}={"userWord.difficulty":"hard"}`;
-    const response = await fetch(url);
-    const data: IWord[] = await response.json();
-    return data;
+  public async getDifficultWords({
+    userId,
+    token,
+  }: IRequestParameters): Promise<IAggregatedWord[]> {
+    const url = `${BASE_URL}/${PATHS.users}/${userId}/${PATHS.aggregatedWords}?${QUERY_KEYS.wordsPerPage}=${MAX_WORDS_IN_BOOK}&${QUERY_KEYS.filter}={"userWord.difficulty":"hard"}`;
+    const response = await fetch(url, {
+      method: HttpMethods.GET,
+      headers: {
+        [REQUEST_HEADERS.authorization]: `Bearer ${token}`,
+        [REQUEST_HEADERS.accept]: 'application/json',
+      },
+    });
+    const data = await response.json();
+    const difficultWords = data[Numbers.Zero].paginatedResults;
+    return difficultWords;
   }
 }
