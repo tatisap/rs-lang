85631d499e7820aa75c960c87bb5ddd9b4174728
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-09-02 12:32:03 +0300

    feat: add result processor

---
+++ src/components/games/common/result-processor.ts
@@ -0,0 +1,119 @@
+import WordsAPI from '../../../api/words-api';
+import { GameName, IGameCorrectAnswer, IGameQuestionResult, IUserWord } from '../../../types';
+import DateFormatter from '../../../utils/date-formatter';
+import RequestProcessor from '../../request-processor';
+import UserWord from './user-word';
+
+export default class GameResultProcessor {
+  private api: WordsAPI;
+
+  private requestProcessor: RequestProcessor;
+
+  private dateFormatter: DateFormatter;
+
+  constructor() {
+    this.api = new WordsAPI();
+    this.requestProcessor = new RequestProcessor();
+    this.dateFormatter = new DateFormatter();
+  }
+
+  public async processAnswer(gameName: GameName, answer: IGameQuestionResult): Promise<void> {
+    const wordInfo: IGameCorrectAnswer = answer.correctAnswer;
+    const todayDateKey: string = this.dateFormatter.getStringifiedDateKey(new Date());
+
+    console.log(answer);
+
+    if (answer.isCorrect) {
+      await this.processCorrectAnswer(wordInfo, todayDateKey, gameName);
+    } else {
+      await this.processIncorrectAnswer(wordInfo, todayDateKey, gameName);
+    }
+  }
+
+  private async processCorrectAnswer(
+    word: IGameCorrectAnswer,
+    dateKey: string,
+    gameName: GameName
+  ): Promise<void> {
+    try {
+      const userWordInfo: IUserWord = await this.requestProcessor.process<IUserWord>(
+        this.api.getUserWord,
+        { wordId: word.wordId }
+      );
+
+      const userWord: UserWord = new UserWord().update(userWordInfo);
+
+      userWord.increaseCorrectAnswersInRow();
+
+      if (!userWord.getLearningStatus() && userWord.checkCorrectAnswersInRow()) {
+        userWord.markAsLearned(dateKey);
+      }
+
+      if (userWord.isNew()) {
+        userWord.setFirstUseInfo(gameName, dateKey);
+      }
+
+      userWord.increaseCorrectAnswers(gameName, dateKey);
+
+      console.log(userWord.getUserWordInfo());
+      await this.requestProcessor.process(this.api.updateUserWord, {
+        wordId: word.wordId,
+        body: userWord.getUserWordInfo(),
+      });
+      console.log('1');
+    } catch (err) {
+      console.log(err);
+      const userWord: UserWord = new UserWord();
+
+      userWord.increaseCorrectAnswersInRow();
+      userWord.setFirstUseInfo(gameName, dateKey);
+      userWord.increaseCorrectAnswers(gameName, dateKey);
+
+      console.log(userWord);
+      await this.requestProcessor.process(this.api.createUserWord, {
+        wordId: word.wordId,
+        body: userWord.getUserWordInfo(),
+      });
+    }
+  }
+
+  private async processIncorrectAnswer(
+    word: IGameCorrectAnswer,
+    dateKey: string,
+    gameName: GameName
+  ): Promise<void> {
+    try {
+      const userWordInfo: IUserWord = await this.requestProcessor.process<IUserWord>(
+        this.api.getUserWord,
+        {
+          wordId: word.wordId,
+        }
+      );
+      const userWord: UserWord = new UserWord().update(userWordInfo);
+      userWord.resetCorrectAnswersInRow();
+      if (userWord.getLearningStatus()) {
+        userWord.remoreLearnedMark();
+      }
+      if (userWord.isNew()) {
+        userWord.setFirstUseInfo(gameName, dateKey);
+      }
+      userWord.increaseIncorrectAnswers(gameName, dateKey);
+
+      await this.requestProcessor.process(this.api.updateUserWord, {
+        wordId: word.wordId,
+        body: userWord.getUserWordInfo(),
+      });
+    } catch (err) {
+      console.log(err);
+      const userWord = new UserWord();
+      userWord.setFirstUseInfo(gameName, dateKey);
+      userWord.increaseIncorrectAnswers(gameName, dateKey);
+
+      console.log(userWord);
+      await this.requestProcessor.process(this.api.createUserWord, {
+        wordId: word.wordId,
+        body: userWord.getUserWordInfo(),
+      });
+    }
+  }
+}
