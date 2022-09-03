1b639354ccde67e11da6a8ec982178dea63b7665
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-09-03 21:44:00 +0300

    feat: cache sprint background

--- src/utils/cacher.ts
+++ src/utils/cacher.ts
@@ -1,8 +1,14 @@
 import audiocallBackground from '../assets/img/game-landscape.jpeg';
+import sprintBackground from '../assets/img/game-waterfall.jpeg';
 import mainBackground from '../assets/img/main-page.png';
 import gamePageBackground from '../assets/img/game-page.png';
 
-const imageSources: string[] = [audiocallBackground, mainBackground, gamePageBackground];
+const imageSources: string[] = [
+  audiocallBackground,
+  sprintBackground,
+  mainBackground,
+  gamePageBackground,
+];
 
 export default class Cacher {
   public preloadPictures(): void {
