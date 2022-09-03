cefa063a1c825ced26e6538fe0a14247e2a1965c
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-09-03 15:59:00 +0300

    feat: add statistic api method

--- src/api/statistic-api.ts
+++ src/api/statistic-api.ts
@@ -1,5 +1,5 @@
-import { HttpMethods, IRequestParameters, IUserStatistics } from '../types';
-import { BASE_URL, PATHS, REQUEST_HEADERS } from '../constants';
+import { HttpMethods, IRequestParameters, IUserStatistics, StatusCode } from '../types';
+import { BASE_URL, ERRORS_MESSAGES, PATHS, REQUEST_HEADERS } from '../constants';
 
 export default class StatisticAPI {
   public async getUserStatistic({ userId, token }: IRequestParameters): Promise<IUserStatistics> {
@@ -13,7 +13,28 @@ export default class StatisticAPI {
         },
       }
     );
+    if (response.status === StatusCode.Unauthorized) {
+      throw new Error(ERRORS_MESSAGES.unauthorized);
+    }
     const statistics: IUserStatistics = (await response.json()) as IUserStatistics;
     return statistics;
+  }
+
+  public async updateUserStatistic({ userId, token, body }: IRequestParameters): Promise<void> {
+    const response: Response = await fetch(
+      `${BASE_URL}/${PATHS.users}/${userId}/${PATHS.statistics}`,
+      {
+        method: HttpMethods.PUT,
+        headers: {
+          [REQUEST_HEADERS.authorization]: `Bearer ${token}`,
+          [REQUEST_HEADERS.accept]: 'application/json',
+          [REQUEST_HEADERS.contentType]: 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    if (response.status === StatusCode.Unauthorized) {
+      throw new Error(ERRORS_MESSAGES.unauthorized);
+    }
   }
 }
